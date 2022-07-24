5b7754e219409d8282d104e0fbf81f2077879448
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-24 15:38:11 -0700

    cleaned up OrderTest file

--- Invoice.Solution/Invoice.Tests/ModelTests/OrderTests.cs
+++ Invoice.Solution/Invoice.Tests/ModelTests/OrderTests.cs
@@ -8,7 +8,6 @@ namespace Invoice.Tests
   [TestClass]
   public class OrderTests : IDisposable
   {
-
     public void Dispose()
     {
       Order.ClearAll();
@@ -22,68 +21,46 @@ namespace Invoice.Tests
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
-
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
-      //Arrange
       string description = "Walk the dog.";
       DateTime date = new DateTime(2008,04,14);
-
-      //Act
       Order newOrder = new Order("testTitle", description, 10, date);
       string result = newOrder.Description;
-
-      //Assert
       Assert.AreEqual(description, result);
     }
 
     [TestMethod]
     public void SetDescription_SetDescription_String()
     {
-      //Arrange
       string description = "Walk the dog.";
       DateTime date = new DateTime(2008,04,14);
       Order newOrder = new Order("testTitle", description, 10, date);
-      
-
-      //Act
       string updatedDescription = "Do the dishes";
       newOrder.Description = updatedDescription;
       string result = newOrder.Description;
-
-      //Assert
       Assert.AreEqual(updatedDescription, result);
     }
 
     [TestMethod]
     public void GetAll_ReturnsEmptyList_OrderList()
     {
-      // Arrange
       List<Order> newList = new List<Order> { };
-
-      // Act
       List<Order> result = Order.GetAll();
-
-      // Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
     [TestMethod]
     public void GetAll_ReturnsOrders_OrderList()
     {
-      //Arrange
       string description01 = "Walk the dog";
       string description02 = "Wash the dishes";
       DateTime date = new DateTime(2008,04,14);
       Order newOrder1 = new Order("testTitle1", description01, 10, date);
       Order newOrder2 = new Order("testTitle2", description02, 10, date);
       List<Order> newList = new List<Order> { newOrder1, newOrder2 };
-
-      //Act
       List<Order> result = Order.GetAll();
-
-      //Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
@@ -100,17 +77,12 @@ namespace Invoice.Tests
     [TestMethod]
     public void Find_ReturnsCorrectItem_Order()
     {
-      //Arrange
       string description01 = "Walk the dog";
       string description02 = "Wash the dishes";
       DateTime date = new DateTime(2008,04,14);
       Order newOrder1 = new Order("testTitle1", description01, 10, date);
       Order newOrder2 = new Order("testTitle2", description02, 10, date);
-
-      //Act
       Order result = Order.Find(2);
-
-      //Assert
       Assert.AreEqual(newOrder2, result);
     }
   }
